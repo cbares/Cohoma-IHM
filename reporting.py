cfbe8f29b8126bb7abc86c76d38d419f2b4b7bdf
cbares <38895035+cbares@users.noreply.github.com>
2023-04-17 17:22:19 +0200

    use session for speed up network transmissions

--- reporting.py
+++ reporting.py
@@ -11,7 +11,7 @@ curl -X 'GET' \
 team = "theseus"
 auth = "5fyg-mqqs-rxqu-sfjm-pl2d"
 ODAC = "https://6bus5bof45.execute-api.eu-west-3.amazonaws.com/dev/trackers"
-ODAC = "http://127.0.0.1:8000/dev/trackers"
+# ODAC = "http://127.0.0.1:8000/dev/trackers"
 
 DELTA = 1 # seconde
 
@@ -22,23 +22,42 @@ header = {'accept': 'application/json'}
 URL_IHM = f"http://{SERVER}:{PORT}/{URI}"
 
 
-def update_data_ODAC():
-    result = requests.get(URL_IHM, header)
-    marker = result.json()
+def spinning_cursor():
+    while True:
+        for cursor in '|/-\\':
+            yield cursor
 
+spinner = spinning_cursor()
+
+
+def update_data_ODAC(s_src, s_dst):
+    global spinner
+    result = s_src.get(URL_IHM, headers=header)
+    marker = result.json()
+    c = 0
     for m in marker:
         m["team"] = team
         m["auth"] = auth
-        r = requests.post(ODAC, json=m)
-        if r.status_code != requests.codes.ok:
+        m["timestamp"] = int(time.time() * 1000)
+        r = s_dst.post(ODAC, json=m)
+        if r.status_code == requests.codes.ok:
+            c = c + 1
+        else:
             print(r.status_code, m)
+        #time.sleep(0.1)
+    if c == len(marker):
+        print(next(spinner), end='\b', flush=True)
+    else:
+        print("x", end='', flush=True)
 
 
-def schedule_udpate_data(scheduler):
-    scheduler.enter(delay=DELTA, priority=1, action=schedule_udpate_data, argument=(scheduler,))
-    update_data_ODAC()
+def schedule_udpate_data(scheduler, s_src, s_dst):
+    scheduler.enter(delay=DELTA, priority=1, action=schedule_udpate_data, argument=(scheduler, s_src, s_dst))
+    update_data_ODAC(s_src, s_dst)
 
 
+s_odac = requests.Session()
+s_theseus = requests.Session()
 cohoma_scheduler = sched.scheduler(time.time, time.sleep)
-schedule_udpate_data(cohoma_scheduler)
+schedule_udpate_data(cohoma_scheduler, s_theseus, s_odac)
 cohoma_scheduler.run()
