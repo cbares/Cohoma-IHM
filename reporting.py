e6d79fc6d25c8ce8d338f832de2d1d8162d33494
cbares <38895035+cbares@users.noreply.github.com>
2023-04-17 03:22:24 +0200

    report every seconde

--- reporting.py
+++ reporting.py
@@ -1,4 +1,5 @@
 #!python
+import sched, time
 
 import requests
 
@@ -17,11 +18,26 @@ PORT = "8000"
 URI = "api/waypoint/reporting"
 header = {'accept': 'application/json'}
 
-result = requests.get(f"http://{SERVER}:{PORT}/{URI}", header)
-marker = result.json()
+def update_data_ODAC():
+    result = requests.get(f"http://{SERVER}:{PORT}/{URI}", header)
+    marker = result.json()
 
-for m in marker:
-    m["team"] = team
-    m["auth"] = auth
-    requests.post(ODAC, json=m)
+    for m in marker:
+        m["team"] = team
+        m["auth"] = auth
+        r = requests.post(ODAC, json=m)
+        if r.status_code != requests.codes.ok:
+            print(r.status_code, m)
 
+
+def do_something(scheduler):
+    # schedule the next call first
+    scheduler.enter(1, 1, do_something, (scheduler,))
+    # then do your stuff:
+    print(".",end='')
+    update_data_ODAC()
+
+
+my_scheduler = sched.scheduler(time.time, time.sleep)
+my_scheduler.enter(2, 1, do_something, (my_scheduler,))
+my_scheduler.run()
