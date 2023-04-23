b06ee3de3ee4246127fe0f679796cc1ca93a8e9e
cbares <38895035+cbares@users.noreply.github.com>
2023-04-23 10:22:38 +0200

    websockets simple exchange

--- apis/base.py
+++ apis/base.py
@@ -3,9 +3,10 @@ from fastapi import APIRouter
 from apis.version1 import route_general_pages
 from apis.version1 import route_markers
 from apis.version1 import route_baptemes
-
+from apis.version1 import route_websockets
 
 api_router = APIRouter()
 api_router.include_router(route_general_pages.general_pages_router, prefix="", tags=["general_pages"])
 api_router.include_router(route_markers.router, prefix="/api/waypoint", tags=["markers"])
 api_router.include_router(route_baptemes.router, prefix="/api/baptemes", tags=["baptemes"])
+api_router.include_router(route_websockets.router, prefix="", tags=["ws"])
